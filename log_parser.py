65e9d01c41eb7e2be8147ecd1e0f4d99462c9bdb
Hafeeza Abdur Rehman <avoid.me.404@gmail.com>
2026-09-09 15:34:15 +0500

    Delete log_parser.py

--- log_parser.py
+++
@@ -1,8 +0,0 @@
-count = 0
-
-with open("sample.log", "r") as file:
-    for line in file:
-        if "failed" in line:
-            count = count + 1
-
-print("Number of failed login attempts:", count)
